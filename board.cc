393caa6abf967351dc79e339e1ea81993fb9bf68
Adrian Santo Pasqualini <aspasqualini@uwaterloo.ca>
2019-07-24 15:10:30 -0400

    Work on grid init

--- board.cc
+++ board.cc
@@ -2,4 +2,13 @@
 using namespace std;
 
 
+void Board::init(string playerOne, string playerTwo) {
+	whitesTurn = true;
+	squares.clear();
+	for (int r = 0; i < 8; i++) {
+		for (int c = 0; c < 8; c++) {
+			squares[r].emplace_back(Square(r,c));
+		}
+	}
+}
 
