b8b2e41f0677d483aa13a3a93048f83e682186a5
William Wang <WilliamWang@v1040-wn-rt-c-35-153.campus-dynamic.uwaterloo.ca>
2019-07-24 17:07:10 -0400

    cleanup

--- piece.cc
+++ piece.cc
@@ -1,7 +1,6 @@
 #include "piece.h"
 using namespace std;
 
-<<<<<<< HEAD
 Piece::Piece(int row, int col, int movesMade, bool isWhite, string id):
 	row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
 
@@ -46,12 +45,3 @@ King::King(int row, int col, int movesMade, bool isWhite, string id):
 void King::move(int r, int c) {
 
 }
-=======
-<<<<<<< HEAD
-Piece::Piece(int row, int col, string id, bool isWhite, int movesMade): 
-	row {row}, col {col}, id {id}, isWhite {isWhite}, movesMade {movesMade} {};
-=======
-Piece(int row, int col, int movesMade, bool isWhite, string id);
-	row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {};
->>>>>>> 3b16f3ec1d140829028b45118f0d4837137b17b9
->>>>>>> a4aa4d5cf1cfbfd4facac90feef4044122e2b2a9
