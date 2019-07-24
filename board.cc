37eee05f3480bdf3765628f644f0b09a3e9a4cda
William Wang <WilliamWang@v1040-wn-rt-c-35-153.campus-dynamic.uwaterloo.ca>
2019-07-24 15:11:56 -0400

    board.cc output

--- board.cc
+++ board.cc
@@ -1,5 +1,13 @@
 #include "board.h"
 using namespace std;
 
-
-
+ostream & operator<<(ostream &out, const Board &b) {
+	for (int i = 0; i < 8; ++i) {
+		for (int j = 0; j < 8; ++j) {
+			Piece piece = b.squares[i][j].currPiece;
+			out << piece.id[0];
+		}
+		out << endl;
+	}
+	return out;
+}
