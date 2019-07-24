3ebec478f0d43efa5c31b9d52a95777391d2a5bd
William Wang <WilliamWang@v1040-wn-rt-c-35-153.campus-dynamic.uwaterloo.ca>
2019-07-24 17:02:19 -0400

    placeholders

--- piece.cc
+++ piece.cc
@@ -1,5 +1,47 @@
 #include "piece.h"
 using namespace std;
 
-Piece(int row, int col, int movesMade, bool isWhite, string id);
-	row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {};
+Piece::Piece(int row, int col, int movesMade, bool isWhite, string id):
+	row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+Pawn::Pawn(int row, int col, int movesMade, bool isWhite, string id, bool specialAdvance):
+	row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id}, specialAdvance {specialAdvance} {}
+
+void Pawn::move(int r, int c) {
+	
+}
+
+Knight::Knight(int row, int col, int movesMade, bool isWhite, string id):
+        row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+void Knight::move(int r, int c) {
+
+}
+
+Bishop::Bishop(int row, int col, int movesMade, bool isWhite, string id):
+        row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+void Bishop::move(int r, int c) {
+
+}
+
+Rook::Rook(int row, int col, int movesMade, bool isWhite, string id):
+        row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+void Rook::move(int r, int c) {
+
+}
+
+Queen::Queen(int row, int col, int movesMade, bool isWhite, string id):
+        row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+void Queen::move(int r, int c) {
+
+}
+
+King::King(int row, int col, int movesMade, bool isWhite, string id):
+        row {row}, col {col}, movesMade {movesMade}, isWhite {isWhite}, id {id} {}
+
+void King::move(int r, int c) {
+
+}
--- piece.h
+++ piece.h
@@ -18,52 +18,46 @@ class Piece {
 		}
 		
 		Piece(int row, int col, int movesMade = 0, bool isWhite, string id);
-		//virtual void move(int r, int c) = 0;
+		virtual void move(int r, int c) = 0;
 
 };
 
-/*
 class Pawn: public Piece {
 	bool specialAdvance;
 	
 	public:
-		Pawn(int row, int col, string id, bool isWhite, int movesMade = 0, bool specialAdvance = false);
+		Pawn(int row, int col, int movesMade = 0, bool isWhite, string id, bool specialAdvance = false);
 		void move(int r, int c) override;
 };
 
-
 class Knight: public Piece {
         public:
-                Knight(int row, int col, string id, bool isWhite, int movesMade = 0);
-                void move(int r, int c) override;
+                Knight(int row, int col, int movesMade = 0, bool isWhite, string id);
+		void move(int r, int c) override;
 };
-
 
 class Bishop: public Piece {
         public:
-                Bishop(int row, int col, string id, bool isWhite, int movesMade = 0);
-                void move(int r, int c) override;
+                Bishop(int row, int col, int movesMade = 0, bool isWhite, string id);
+		void move(int r, int c) override;
 };
-
 
 class Rook: public Piece {
         public:
-                Rook(int row, int col, string id, bool isWhite, int movesMade = 0);
-                void move(int r, int c) override;
+                Rook(int row, int col, int movesMade = 0, bool isWhite, string id);
+		void move(int r, int c) override;
 };
-
 
 class Queen: public Piece {
         public:
-                Queen(int row, int col, string id, bool isWhite, int movesMade = 0);
-                void move(int r, int c) override;
+                Queen(int row, int col, int movesMade = 0, bool isWhite, string id);
+		void move(int r, int c) override;
 };
-
 
 class King: public Piece {
         public:
-                King(int row, int col, string id, bool isWhite, int movesMade = 0);
-                void move(int r, int c) override;
+                King(int row, int col, int movesMade = 0, bool isWhite, string id);
+		void move(int r, int c) override;
 };
-*/
+
 #endif
