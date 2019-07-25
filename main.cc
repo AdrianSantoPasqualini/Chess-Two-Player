02dd525ad765c013043577e2bb903bad1a7c59e1
William Wang <WilliamWang@Williams-MacBook-Pro.local>
2019-07-25 13:17:52 -0400

    fixed tabs

--- main.cc
+++ main.cc
@@ -4,28 +4,25 @@
 using namespace std;
 
 int main() {
-  cin.exceptions(ios::eofbit|ios::failbit);
-  string cmd;
-  Board board;
-  try {
-    while (true) {
-      cin >> cmd;
-      if (cmd == "game") {
-        string p1, p2;
-        cmd >> p1;
-        cmd >> p2; 
-        board.init();
-      }
-      else if (cmd == "resign") {
-        //resign
-      }
-      else if (cmd == "move") {
-        //move
-      }
-      else if (cmd == "setup") {
-        //setup
-        //user cannot leave setup mode until conditions are met
-      }
-    }
-  }
+	cin.exceptions(ios::eofbit|ios::failbit);
+	string cmd;
+  	Board board;
+	try {
+		while (true) {
+			cin >> cmd;
+			if (cmd == "game") {
+				string p1, p2;
+				cmd >> p1;
+				cmd >> p2; 
+				board.init();
+			} else if (cmd == "resign") {
+				//resign
+			} else if (cmd == "move") {
+				//move
+			} else if (cmd == "setup") {
+				//setup
+				//user cannot leave setup mode until conditions are met
+			}
+		}
+	}
 }
