b29c40add21d6492cc6a02d1c43fa7e4ca613d53
William Wang <WilliamWang@Williams-MacBook-Pro.local>
2019-07-25 14:48:21 -0400

    a line

--- square.h
+++ square.h
@@ -12,14 +12,13 @@ class Square {
 	vector<shared_ptr<Piece>> wAttacks;
 	vector<shared_ptr<Piece>> bAttacks;
 	shared_ptr<Piece> currPiece;
-//	Piece * currPiece;
 	int r;
 	int c;
 	
 	public:
 		shared_ptr<Piece> getPiece() const;
 		void setPiece(shared_ptr<Piece> piece);
-		Square(int r, int c, shared_ptr<Piece> currPiece);  
+		Square(int r, int c, shared_ptr<Piece> currPiece); 
 		//void notify() override;
 };
 #endif
