c009b9f6405274fe5a1b8964920e95e33de1d8ee
William Wang <WilliamWang@Williams-MacBook-Pro.local>
2019-07-25 14:36:51 -0400

    piece move functions

--- piece.cc
+++ piece.cc
@@ -13,28 +13,53 @@ void Piece::changeCoor(int r, int c) {
 	col = c;
 }
 
+bool Piece::getIsWhite() {
+	return isWhite;
+}
+
 string Piece::getId() {
 	return id;
+}
+
+int Piece::getMovesMade() {
+	return movesMade;
 }
 
 Pawn::Pawn(int row, int col, bool isWhite, string id, int movesMade, bool specialAdvance): 
 	Piece{row, col, isWhite, id, movesMade}, specialAdvance {specialAdvance} {}
 
-void Pawn::move(int r, int c) {
-		
+void Pawn::move(int r, int c, bool cover) {
+	pair<int,int> currCoor = getCoor();
+        int movesMade = getMovesMade();
+	if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
+                throw;  
+        }
+	int dir = 1;
+	if (!getIsWhite()) {
+		dir = -1;	
+	}
+	if (currCoor.first - r == dir * 1 && currCoor.second - c == 0) {
+                changeCoor(r, c);
+	} else if ((movesMade == 0) && (currCoor.first - r == dir * 2 && currCoor.second - c == 0)) {
+		changeCoor(r, c);
+	} else if ((currCoor.first - r == dir * 1 && abs(currCoor.second - c == 1)) && cover) {
+		changeCoor(r, c);
+	} else {
+		throw;
+	}
 }
 
 Knight::Knight(int row, int col, bool isWhite, string id, int movesMade):
         Piece{row, col, isWhite, id, movesMade} {}
 
-void Knight::move(int r, int c) {
+void Knight::move(int r, int c, bool cover) {
 	pair<int,int> currCoor = getCoor();
-	if (r < 0 || r > 7 || c < 0 || c > 7) {
+	if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
 		throw;
 	}
 	if ((abs(currCoor.first - r) == 2 && abs(currCoor.second - c) == 1) ||
 	    (abs(currCoor.first - r) == 1 && abs(currCoor.second - c) == 2)) {
-		changeCoor(r, c);	
+		changeCoor(r, c);
 	} else {
 		throw;
 	}
@@ -43,9 +68,9 @@ void Knight::move(int r, int c) {
 Bishop::Bishop(int row, int col, bool isWhite, string id, int movesMade):
         Piece{row, col, isWhite, id, movesMade} {}
 
-void Bishop::move(int r, int c) {
+void Bishop::move(int r, int c, bool cover) {
 	pair<int,int> currCoor = getCoor();
-        if (r < 0 || r > 7 || c < 0 || c > 7) {
+        if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
                 throw;
         }
 	if ((abs(currCoor.first - r) == abs(currCoor.second - c)) &&
@@ -59,9 +84,9 @@ void Bishop::move(int r, int c) {
 Rook::Rook(int row, int col, bool isWhite, string id, int movesMade):
         Piece{row, col, isWhite, id, movesMade} {}
 
-void Rook::move(int r, int c) {
+void Rook::move(int r, int c, bool cover) {
 	pair<int,int> currCoor = getCoor();
-        if (r < 0 || r > 7 || c < 0 || c > 7) {
+        if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
                 throw;
         }
 	if ((abs(currCoor.first - r) == 0 && abs(currCoor.second - c) > 0) ||
@@ -75,9 +100,9 @@ void Rook::move(int r, int c) {
 Queen::Queen(int row, int col, bool isWhite, string id, int movesMade):
         Piece{row, col, isWhite, id, movesMade} {}
 
-void Queen::move(int r, int c) {
+void Queen::move(int r, int c, bool cover) {
 	pair<int,int> currCoor = getCoor();
-        if (r < 0 || r > 7 || c < 0 || c > 7) {
+        if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
                 throw;
         }
 	if (((abs(currCoor.first - r) == abs(currCoor.second - c)) &&
@@ -93,9 +118,9 @@ void Queen::move(int r, int c) {
 King::King(int row, int col, bool isWhite, string id, int movesMade):
         Piece{row, col, isWhite, id, movesMade} {}
 
-void King::move(int r, int c) {
+void King::move(int r, int c, bool cover) {
 	pair<int,int> currCoor = getCoor();
-        if (r < 0 || r > 7 || c < 0 || c > 7) {
+        if (r < 0 || r > 7 || c < 0 || c > 7 || cover) {
                 throw;
         }
 	if ((currCoor.first - r) * (currCoor.first - r) + (currCoor.second - c) * (currCoor.second - c) <= 2) {
--- piece.h
+++ piece.h
@@ -16,9 +16,11 @@ class Piece {
 	public:
 		pair<int,int> getCoor();
 		void changeCoor(int r, int c);
+		bool getIsWhite();
 		string getId();
+		int getMovesMade();
 		Piece(int row, int col, bool isWhite, string id, int movesMade);
-		virtual void move(int r, int c) = 0;
+		virtual void move(int r, int c, bool cover) = 0;
 
 };
 
@@ -26,37 +28,37 @@ class Pawn final: public Piece {
 	bool specialAdvance;
 	public:
 		Pawn(int row, int col, bool isWhite, string id, int movesMade = 0, bool specialAdvance = false);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 class Knight final: public Piece {
         public:
                 Knight(int row, int col, bool isWhite, string id, int movesMade = 0);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 class Bishop final: public Piece {
         public:
                 Bishop(int row, int col, bool isWhite, string id, int movesMade = 0);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 class Rook final: public Piece {
         public:
                 Rook(int row, int col, bool isWhite, string id, int movesMade = 0);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 class Queen final: public Piece {
         public:
                 Queen(int row, int col, bool isWhite, string id, int movesMade = 0);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 class King final: public Piece {
         public:
                 King(int row, int col, bool isWhite, string id, int movesMade = 0);
-		void move(int r, int c) override;
+		void move(int r, int c, bool cover) override;
 };
 
 #endif
