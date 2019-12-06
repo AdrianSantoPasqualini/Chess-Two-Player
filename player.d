4fd0d8f28962dda7e930577f729c18404d487c32
Adrian Santo Pasqualini <aspasqualini@uwaterloo.ca>
2019-12-05 21:17:32 -0500

    Removing dependencies and exectuable files

--- player.d
+++
@@ -1,4 +0,0 @@
-player.o: player.cc player.h move.h piece.h board.h square.h observer.h \
-  subject.h state.h window.h /usr/X11/include/X11/Xlib.h \
-  /usr/X11/include/X11/X.h /usr/X11/include/X11/Xfuncproto.h \
-  /usr/X11/include/X11/Xosdefs.h info.h
